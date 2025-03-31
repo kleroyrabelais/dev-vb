a0af085165d29a80ac80d6111dd8cbc1e966ffca
sio1 <sio1@c106-03.rai.local>
2025-03-31 15:42:34 +0200

    dernicer changement de produit

--- essai/objets/2412.vb
+++ essai/objets/2412.vb
@@ -1,7 +1,13 @@
-﻿Module _2412
-    Dim unProduit As New Hashtable
+﻿Imports System.Runtime.CompilerServices
+
+Module _2412
+    Dim lesProduits As New Hashtable
+    Dim unProduit As Produit
     Sub main()
         Dim choix As Integer
+        Dim designation, ReferenceP As String
+        Dim prixHT, TauxTVA As Double
+
 
         Do
             Console.WriteLine("1. Ajouter un produit au catalogue")
@@ -20,30 +26,44 @@
             Select Case choix
                 Case 1
                     Console.WriteLine("saisir la designation du produit")
+                    designation = Console.ReadLine()
                     Console.WriteLine("saisir le prix HT")
+                    prixHT = Console.ReadLine()
                     Console.WriteLine("saisir le taux de TVA")
+                    TauxTVA = Console.ReadLine()
                     Console.WriteLine("saisir la référence du produit")
-                    AjouterProduit()
+                    ReferenceP = Console.ReadLine()
+                    unProduit = New Produit
+
+
+
                 Case 2
                     Console.WriteLine("saisir prix HT (supérieur a celui de base)")
+                    prixHT = Console.ReadLine()
                     ModifierPrix()
                 Case 3
                     Console.WriteLine("saisir prix HT (inférieure a celui de base)")
+                    prixHT = Console.ReadLine()
                     ModifierPrix()
                 Case 4
                     Console.WriteLine("saisir la designation du produit")
+                    designation = Console.ReadLine()
                     Console.WriteLine("saisir pour modifier TVA")
+                    TauxTVA = Console.ReadLine()
                     ModifierTVA()
                 Case 5
                     Console.WriteLine("saisir un nombre pour augmenter les prix de tant")
+                    prixHT = Console.ReadLine()
                     AugmenterTousLesPrix()
                 Case 6
                     Console.WriteLine("saisir la designation du produit")
+                    designation = Console.ReadLine()
                     SupprimerProduit()
                 Case 7
                     AfficherCatalogue()
                 Case 8
                     Console.WriteLine("saisir la designation du produit")
+                    designation = Console.ReadLine()
                     AfficherProduit()
                 Case 9
                     ViderCatalogue()
@@ -53,3 +73,4 @@
         Loop Until choix = 10
     End Sub
 End Module
+
